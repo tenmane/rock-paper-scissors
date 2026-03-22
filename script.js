f08546af3d4d16537dd90ebb3bd27e62854318d8
tenmane <235837458+tenmane@users.noreply.github.com>
2026-03-23 01:10:35 +0400

    Implement playRound function with scores Checks all scenarios in rock-paper-scissors Updates humanScore and computerScore after each round

--- script.js
+++ script.js
@@ -1,29 +1,55 @@
 let humanScore = 0;
 let computerScore = 0;
 
-function getComputerChoice(){
-  return Math.floor(Math.random()*3)+1;
+function getComputerChoice() {
+  return Math.floor(Math.random() * 3) + 1;
 }
 let computerChoice = getComputerChoice();
-function getHumanChoice(n){
-  n=n.toLowerCase();
-  if(n==="rock"){
+console.log(computerChoice);
+function getHumanChoice(n) {
+  n = n.toLowerCase();
+  if (n === "rock") {
     return 1;
   }
-  else if (n==="paper"){
+  else if (n === "paper") {
     return 2;
   }
-  else if (n==="scissors"){
+  else if (n === "scissors") {
     return 3;
   }
-  else{
+  else {
     return "invalid input.";
   }
 }
-let pick = prompt("Enter your choice: ","");
+let pick = prompt("Enter your choice: ", "");
 let humanChoice = getHumanChoice(pick);
-console.log(humanChoice);
 
-function playRound(humanChoice, computerChoice){
-  
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    alert("Draw!");
+  }
+  else if (humanChoice == 1 && computerChoice == 2) {
+    computerScore += 1;
+    alert(`Computer wins! Current Score #Computer: ${computerScore} #You: ${humanScore}`);
+  }
+  else if (humanChoice == 2 && computerChoice == 1) {
+    humanScore += 1;
+    alert(`You win! Current Score  #Computer: ${computerScore} #You: ${humanScore}`);
+  }
+  else if (humanChoice == 1 && computerChoice == 3) {
+    humanScore += 1;
+    alert(`You win! Current Score  #Computer: ${computerScore} #You: ${humanScore}`);
+  }
+  else if (humanChoice == 3 && computerChoice == 1) {
+    computerScore += 1;
+    alert(`Computer wins! Current Score #Computer: ${computerScore} #You: ${humanScore}`);
+  }
+  else if (humanChoice == 2 && computerChoice == 3) {
+    computerScore += 1;
+    alert(`Computer wins! Current Score #Computer: ${computerScore} #You: ${humanScore}`);
+  }
+  else {
+    humanScore += 1;
+    alert(`You win! Current Score  #Computer: ${computerScore} #You: ${humanScore}`);
+  }
 }
